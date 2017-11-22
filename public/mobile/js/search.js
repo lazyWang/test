c4b9c4d70873cb1d043551b7d5fce3d7b680dc4b
why <why@qq.com>
2017-11-22 21:44:06 +0800

    第一天

---
+++ public/mobile/js/search.js
@@ -0,0 +1,60 @@
+/**
+ * Created by why on 2017/11/22.
+ */
+
+$(function () {
+    getHistory();
+    function getHistory(){
+        var ls = localStorage;
+        var arr = (ls.getItem("search_history")&&JSON.parse(ls.getItem("search_history")))||[];
+        console.log(arr);
+        if(arr.length<1){
+            $(".history_content").html("没有历史记录");
+            return;
+        }
+
+        var str = [];
+        for(var i = 0 ;i<arr.length;i++){
+            str.push(' <div class="history_list mui-clearfix"> ' +
+                '<span class="mui-pull-left">'+arr[i]+'</span> ' +
+                '<span class="icon_delete fa fa-close mui-pull-right clear_history"></span>' +
+                ' </div>'
+        )
+        }
+        $(".history_content").html(str.join(''))
+    }
+
+    $(".search_btn").on("tap",function () {
+        var val = $.trim($(".search_text").val());
+        if(!val){
+            return false;
+        }
+        var ls = localStorage;
+        var arr = (ls.getItem("search_history")&&JSON.parse(ls.getItem("search_history")))||[];
+
+        for(var i = 0; i<arr.length;i++){
+            if(val==arr[i]){
+                arr.splice(i,1);
+                break;
+            }
+        }
+
+        arr.unshift(val);
+        ls.setItem("search_history",JSON.stringify(arr));
+    })
+
+    $(".clear_historys").on("tap",function () {
+        localStorage.setItem("search_history",JSON.stringify([]));
+        getHistory();
+    })
+    
+    $(".search_history").on("tap",".clear_history",function () {
+        var index = $(this).parent().index();
+        console.log(index);
+        var ls = localStorage;
+        var arr = ls.getItem("search_history")&&JSON.parse(ls.getItem("search_history")) || [];
+        arr.splice(index,1);
+        ls.setItem("search_history",JSON.stringify(arr));
+        getHistory();
+    })
+})
